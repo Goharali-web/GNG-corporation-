f2c5456f64eec22ee169c8b69c1f562b6486454f
Goharali-web <ga480926@gmail.com>
2026-08-06 20:27:42 +0500

    Update js/config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,7 @@
 window.GNG_CONFIG = {
   SUPABASE_URL: "https://xneeljogbzldbdzdccdt.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_T-kioo9_PrSyWANCJD0vHQ_wBSYo_z8",
+  SUPABASE_SERVICE_ROLE_KEY: "your_supabase_service_role_key_here",
   ADMIN_EMAIL: "ga480926@gmail.com",
   ADMIN_PASSWORD: "nM^desWM%?19_)gng"
 };
